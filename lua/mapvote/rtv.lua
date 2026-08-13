ea1f1bc9cd3aae15095fcd9e54fb86d3ef6690ca
Eric Zhang <ericzhang456@disroot.org>
2026-08-13 14:40:02 +0800

    refactor rtv module

--- lua/mapvote/rtv.lua
+++ lua/mapvote/rtv.lua
@@ -1,24 +1,66 @@
 util.AddNetworkString("RTV_Requested")
 
-RTV = RTV or {}
-RTV.ChatCommands = {
+local RTV = {}
+local rtvChatCommands = {
 	"!rtv",
 	"/rtv",
 	"rtv"
 }
-RTV.TotalVotes = 0
-RTV.Wait = MapVote.Config.RTVWaitTime or 60 -- The wait time in seconds. This is how long a player has to wait before voting when the map changes. 
-RTV.Percentage = MapVote.Config.RTVPercentage or 0.66
-RTV._ActualWait = CurTime() + RTV.Wait
-RTV.PlayerCount = MapVote.Config.RTVPlayerCount or 3
+RTV.VotedClients = {}
+
+RTV.GetPercentage = function ()
+	return MapVote.Config.RTVPercentage or 0.66
+end
 
 RTV.ShouldChange = function ()
-	return RTV.TotalVotes >= math.Round(#player.GetAll()*RTV.Percentage)
+	return #RTV.VotedClients >= math.Round(#player.GetAll() * RTV.GetPercentage())
 end
 
-RTV.RemoveVote = function ()
-	RTV.TotalVotes = math.Clamp( RTV.TotalVotes - 1, 0, math.huge )
+RTV.GetWaitTime = function ()
+	return MapVote.Config.RTVWaitTime or 60
 end
+
+RTV._RtvAllowedTime = nil
+
+RTV.ResetWaitTime = function ()
+	RTV._RtvAllowedTime = CurTime() + RTV.GetWaitTime()
+end
+
+RTV.IsRtvAllowedYet = function ()
+	if not RTV._RtvAllowedTime then
+		return true
+	end
+	return RTV._RtvAllowedTime < CurTime()
+end
+
+--- @param ply Player
+--- @return boolean
+RTV.HasClientRtved = function (ply)
+	local userId = ply:UserID()
+	for _, val in ipairs(RTV.VotedClients) do
+		if val == userId then
+			return true
+		end
+	end
+	return false
+end
+
+--- @param ply Player
+RTV.RemoveVote = function (ply)
+	if not RTV.HasClientRtved(ply) then
+		return false
+	end
+
+	local userId = ply:UserID()
+	for i, val in ipairs(RTV.VotedClients) do
+		if val == userId then
+			table.remove(RTV.VotedClients, i)
+			return true
+		end
+	end
+	return false
+end
+
 
 RTV.Start = function ()
 	if GAMEMODE_NAME == "terrortown" then
@@ -35,23 +77,23 @@ RTV.Start = function ()
 		end)
 	else
 		PrintMessage( HUD_PRINTTALK, "#mapvote.rtv_success_now")
-		timer.Simple(4, function()
+		timer.Simple(3, function()
 			MapVote.Start(nil, nil, nil, nil)
 		end)
 	end
 end
 
+--- @param ply Player
 RTV.AddVote = function ( ply )
 	if RTV.CanVote( ply ) then
-		RTV.TotalVotes = RTV.TotalVotes + 1
-		ply.MapVote_RtvRequested = true
+		table.insert(RTV.VotedClients, ply:UserID())
 		ServerLog(ply:Nick() .. " has voted to Rock the Vote.\n")
 		net.Start("RTV_Requested")
 		net.WriteString(ply:Nick())
 		-- current rtv count
-		net.WriteUInt(RTV.TotalVotes, 8)
+		net.WriteUInt(#RTV.VotedClients, 8)
 		-- total player count
-		net.WriteUInt(math.Round(#player.GetAll() * RTV.Percentage), 8)
+		net.WriteUInt(math.Round(#player.GetAll() * RTV.GetPercentage()), 8)
 		net.Broadcast()
 		if RTV.ShouldChange() then
 			RTV.Start()
@@ -61,19 +103,20 @@ end
 
 RTV.CanVote = function ( ply )
 	local plyCount = table.Count(player.GetAll())
-	if RTV._ActualWait >= CurTime() then
+	if not RTV.IsRtvAllowedYet() then
 		return false, "#mapvote.rtv_wait"
 	end
 	if GetGlobalBool( "In_Voting" ) then
 		return false, "#mapvote.rtv_vote_in_progress"
 	end
-	if ply.MapVote_RtvRequested then
+	if RTV.HasClientRtved(ply) then
 		return false, "#mapvote.rtv_voted"
 	end
 	if RTV.ChangingMaps then
 		return false, "#mapvote.rtv_map_changing"
 	end
-	if plyCount < RTV.PlayerCount then
+	local requiedPlayers = MapVote.Config.RTVPlayerCount or 3
+	if plyCount < requiedPlayers then
         return false, "#mapvote.rtv_not_enough_players"
     end
 	return true
@@ -94,17 +137,19 @@ local function TrimString(s)
     return s:match( "^%s*(.-)%s*$" )
 end
 
+hook.Add("Initialize", "MapVoiteRtvResetWaitTime", function ()
+	RTV.ResetWaitTime()
+end)
+
 hook.Add( "PlayerSay", "MapVoteRtvChatCommand", function( ply, text )
-	if table.HasValue( RTV.ChatCommands, TrimString(text:lower()) ) then
+	if table.HasValue( rtvChatCommands, TrimString(text:lower()) ) then
 		RTV.StartVote( ply )
 		return ""
 	end
 end )
 
 hook.Add( "PlayerDisconnected", "MapVoteRemoveRtvOnDisconnect", function( ply )
-	if ply.MapVote_RtvRequested then
-		RTV.RemoveVote()
-	end
+	RTV.RemoveVote(ply)
 	timer.Simple( 0.1, function()
 		if RTV.ShouldChange() then
 			RTV.Start()

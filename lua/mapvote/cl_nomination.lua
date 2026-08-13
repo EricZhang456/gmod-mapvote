ef234ad2a7d7181dfea2000b9ad0f105c3f16366
Eric Zhang <ericzhang456@disroot.org>
2026-08-13 11:34:40 +0800

    add a nomination menu

--- lua/mapvote/cl_nomination.lua
+++ lua/mapvote/cl_nomination.lua
@@ -5,6 +5,69 @@ local mapStatusLookup = {
     [3] = NominationMapStatus.CurrentMap
 }
 
+local itemsPerPage = 5
+
+--- @class NominationMenu
+--- @field maps {name: string, status:NominationMapStatus}[]
+--- @field panel Panel | nil
+local NominationMenu = {}
+NominationMenu.__index = NominationMenu
+
+--- Creates a new nomination menu
+--- @param maplist {name: string, status:NominationMapStatus}[] List of maps to nominate
+--- @return NominationMenu
+function NominationMenu:new(maplist)
+    local obj = setmetatable({}, self)
+    obj.maps = maplist
+    obj.panel = nil
+    return obj
+end
+
+--- Show the menu to the user
+function NominationMenu:show()
+    self.panel = vgui.Create("DFrame")
+    self.panel:Center()
+    self.panel:SetText(language.GetPhrase("mapvote.nomination_menu_title"))
+    self.panel:SetSize(300, 500)
+    self.panel:MakePopup()
+
+    local mapScrollPanel = vgui.Create("DScrollPanel", self.panel)
+    mapScrollPanel:Dock(FILL)
+    for i, val in ipairs(self.maps) do
+        local button = mapScrollPanel:Add("DButton")
+        button:SetHeight(50)
+        local marginTop = 0
+        local marginBottom = 15
+        if i == 1 then
+            marginTop = 5
+        end
+
+        if i == #self.maps then
+            marginBottom = 5
+        end
+
+        button:DockMargin(5, marginTop, 5, marginBottom)
+
+        local mapEnabled = val.status == NominationMapStatus.CanNominate
+        if mapEnabled then
+            button:SetText(val.name)
+            button:SetConsoleCommand("mapvote_nominate", val.name)
+        else
+            local statusStr = ""
+            if val.status == NominationMapStatus.RecentlyPlayed then
+                statusStr = "mapvote.nomination_recently_played"
+            elseif val.status == NominationMapStatus.Nominated then
+                statusStr = "mapvote.nomination_nominated"
+            elseif val.status == NominationMapStatus.CurrentMap then
+                statusStr = "mapvote.nomination_current_map"
+            end
+
+            button:SetText(string.format("%s (%s)", val.name, language.GetPhrase(statusStr)))
+        end
+        button:SetEnabled(mapEnabled)
+    end
+end
+
 net.Receive("Nomination_Requested", function (len, ply)
     local nick = net.ReadString()
     local map = net.ReadString()
@@ -25,5 +88,6 @@ net.Receive("Nomination_MapList", function (len, ply)
         table.insert(maps, {name = mapName, status = mapStatus})
     end
 
-    --- TODO: make a ui and show it to the user
+    local menu = NominationMenu:new(maps)
+    menu:show()
 end)
